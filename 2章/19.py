3d96d431210288421b81155d06b3a81ce04e9d58
K520012C <k520012c@mails.cc.ehime-u.ac.jp>
2025-11-12 15:42:01 +0900

    19.pyの修正

---
+++ 2章/19.py
@@ -0,0 +1,8 @@
+with open('popular-names.txt') as f:
+    lines = f.readlines()
+    num = sorted(lines, key=lambda line: int(line.split('\t')[2]), reverse=True)
+
+    for line in num:
+        print(line,end="")
+
+# sort -k3,3nr popular-names.txt  
